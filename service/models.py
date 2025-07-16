ca88525bb10be4652adc8f8c7fa5ef66152acca5
mohana-shri-v <mohanashrivenkatesan05@gmail.com>
2025-07-16 17:36:14 +0530

    before modification of future !

--- service/models.py
+++ service/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils.text import slugify
 from django.contrib.auth.models import User
 
-# Create your models here.
+# Create your models here.!!
 class Feature(models.Model):
     name=models.CharField(max_length=100)
     details=models.CharField(max_length=500)
